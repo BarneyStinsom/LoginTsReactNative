96a939c62b7b2b086691dc4e16e49234cb95615f
Aluno <Aluno@L3T07.acad.etecpoa>
2026-05-26 09:29:31 -0300

    root

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -4,7 +4,7 @@ export default function RootLayout(){
     return(
         <Tabs>
         <Tabs.Screen name="index" options={{title: "Create Account",}}/>
-        <Tabs.Screen name="poker" options={{title: "Loggin"}}/>
+        <Tabs.Screen name="poker" options={{title: "Logggggin"}}/>
     </Tabs>
     )
 }
